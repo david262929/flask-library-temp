fe545586bb830749ab458029892f0296726461ed
David <davidghazaryan2000@gmail.com>
2020-12-19 15:57:42 +0400

    removed flask-bootsrap from project

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_pymongo import PyMongo
 # from flask_login import LoginManager
@@ -14,7 +13,6 @@ app = Flask(__name__) #, template_folder='../client/build'
 
 app.config.from_object('app.configuration.DevelopmentConfig')
 
-# bs = Bootstrap(app) #flask-bootstrap
 # db = SQLAlchemy(app) #flask-sqlalchemy
 
 # lm = LoginManager()
