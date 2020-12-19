a61942bac349352e2506914c4e6a56e12fee1d3a
David <davidghazaryan2000@gmail.com>
2020-12-19 15:58:30 +0400

    remove unnecessary routes and added wront route handler

--- app/views.py
+++ app/views.py
@@ -12,25 +12,12 @@ from flask import render_template, Response # url_for, redirect, render_template
 from app import app # , lm
 # from app.models import User
 
-def root_dir():  # pragma: no cover
-    return os.path.abspath(os.path.dirname(__file__))
-
-
-def get_file(filename):  # pragma: no cover
-    try:
-        src = os.path.join(root_dir(), filename)
-        # Figure out how flask returns static files
-        # Tried:
-        # - render_template
-        # - send_file
-        # This should not be so non-obvious
-        return open(src).read()
-    except IOError as exc:
-        return str(exc)
+@app.errorhandler(Exception)
+def handle_bad_request(e):
+    return 'bad request!', 404
 
 @app.route('/')
 def index():
-	print('wqwqqqqq')
 	return render_template("index.html", flask_token='token-token-_-token-token')
 
 # @app.route('/', defaults={'path': ''})
